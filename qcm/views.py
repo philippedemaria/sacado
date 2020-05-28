dc2e908703cc20e2e71e510a8a1d6f37d03eda75
Raphael Chay <raphael.chay@gmail.com>
2020-05-28 15:17:06 -0500

    opti prefetch author

--- qcm/views.py
+++ qcm/views.py
@@ -433,8 +433,9 @@ def list_parcours_group(request,id):
 @login_required
 def all_parcourses(request):
     teacher = Teacher.objects.get(user=request.user)
-    parcourses = Parcours.objects.exclude(Q(author=None) | Q(author=teacher), teacher=teacher).filter(linked=0).order_by("author").prefetch_related('exercises__knowledge__theme')
-    #parcourses = parcourses[:15] #limite pour le debuggage
+    parcourses = Parcours.objects.exclude(Q(author=None) | Q(author=teacher), teacher=teacher).filter(
+        linked=0).order_by("author").prefetch_related('exercises__knowledge__theme').select_related('author')
+    # parcourses = parcourses[:15] #limite pour le debuggage
     return render(request, 'qcm/all_parcourses.html', {'parcourses': parcourses})
 
 
