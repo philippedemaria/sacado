88a4f4c0343270d9b19d368ef47e4ab0d41841d8
Philippe Demaria <philippe.demaria83@gmail.com>
2020-05-30 12:05:36 +0100

    ecriture des commentaires sur les fonctions

--- qcm/templatetags/item_tags.py
+++ qcm/templatetags/item_tags.py
@@ -8,70 +8,104 @@ register = template.Library()
 
 @register.filter
 def index(sequence, position):
+    """
+    Renvoie la valeur dans une sequence donné sur un index donné
+    """
     return sequence[position]
 
 
-
-
-@register.simple_tag ## retourne le  score d'un même exercice par élève.
+@register.simple_tag 
 def get_score(obj,student): 
+    """
+    retourne le  score d'un même exercice par élève.
+    """
     return obj.send_score(student) 
 
-@register.simple_tag ## retourne les scores d'un même exercice par élève.
-def get_scores(obj,student): 
+@register.simple_tag 
+def get_scores(obj,student):
+    """
+    retourne les scores d'un même exercice par élève.
+    """ 
     return obj.send_scores(student)
 
-@register.simple_tag ## retourne  l'heure d'un exerice fait par élève.
+@register.simple_tag
 def get_timer(obj,student): 
+    """
+    retourne  l'heure d'un exerice fait par élève.
+    """
     return obj.timer(student) 
 
-@register.simple_tag ## retourne la liste des scores et l'heure d'un exercice fait par élève.
-def get_score_and_time(obj,student): 
+@register.simple_tag ##
+def get_score_and_time(obj,student):
+    """
+    retourne la liste des scores et l'heure d'un exercice fait par élève.
+    """
     return obj.score_and_time(student) 
 
 
-@register.simple_tag ## retourne le dernier score et l'heure d'un exercice fait par élève.
+@register.simple_tag  
 def get_last_score_and_time(obj,parcours,student): 
+    """
+    retourne le dernier score et l'heure d'un exercice fait par élève.
+    """
     return obj.last_score_and_time(parcours,student) 
 
 
 
 
-@register.simple_tag ## retourne si un exercice est choisi dans un parcours.
+@register.simple_tag  
 def get_is_selected(obj,parcours): 
+    """
+    retourne si un exercice est choisi dans un parcours.
+    """
     return obj.is_selected(parcours) 
 
-@register.simple_tag ## retourne le pourcentage d'exercices fait sur un parcours.
+@register.simple_tag 
 def get_percent(obj,student): 
+    """
+    retourne le pourcentage d'exercices fait sur un parcours.
+    """
     return obj.is_percent(student) 
 
 
-@register.simple_tag ## teste le nombre d'exercices faits sur un parcours.
-def get_done(obj,student): 
+@register.simple_tag  
+def get_done(obj,student):
+    """
+    teste le nombre d'exercices faits sur un parcours.
+    """ 
     return obj.is_done(student) 
 
 
-@register.simple_tag ## teste le nombre d'exercices faits sur un parcours.
+@register.simple_tag 
 def get_affect(obj,student): 
+    """
+    teste le nombre d'exercices faits sur un parcours.
+    """
     return obj.is_affect(student) 
 
-# @register.simple_tag ##  Renvoie les exercices selon thème d'un élève 
-# def get_exercises_by_theme(obj,theme): 
-#     return obj.exercises_by_theme(theme) 
+ 
 
-
-@register.simple_tag ##  Renvoie les exercices selon thème d'un élève 
+@register.simple_tag ##  
 def get_resultexercises_by_theme(obj,theme): 
+    """
+    Renvoie les exercices selon thème d'un élève 
+    """
     return obj.resultexercises_by_theme(theme) 
 
 
-@register.simple_tag ##  Renvoie les exercices selon thème d'un élève 
+@register.simple_tag   
 def get_resultknowledge_by_theme(obj,theme): 
+    """
+    Renvoie les exercices selon thème d'un élève
+    """
     return obj.resultknowledge_by_theme(theme) 
 
 
 @register.filter
 def contrast_color(color):
+    """
+    Renvoie la couleur N/B en contraste
+    """
     try :
         color_test = 0.299 * int(color[1:3],16) + 0.587 * int(color[3:5],16) + 0.114 * int(color[5:7],16)
     except :
@@ -92,6 +126,9 @@ def contrast_color(color):
 
 @register.filter
 def cleanhtml(arg):
+    """
+    nettoie le code html
+    """
     cleantext = re.sub('<.*?>', '', arg)
     cleantext = re.sub('\n', '', cleantext)
     cleantext = html.unescape(cleantext)
@@ -99,7 +136,10 @@ def cleanhtml(arg):
 
 
 @register.filter
-def time_done(arg): #convertit 1 entier donné  (en secondes) en durée h:m:s
+def time_done(arg):
+    """
+    convertit 1 entier donné  (en secondes) en durée h:m:s
+    """
     if arg == "" :
         return arg
     else :
@@ -147,7 +187,10 @@ def time_done(arg): #convertit 1 entier donné  (en secondes) en durée h:m:s
 
 
 @register.filter
-def int_minutes(arg): #convertit 1 entier donné (en minutes) en durée
+def int_minutes(arg):  
+    """
+    convertit 1 entier donné (en minutes) en durée
+    """
     if arg == "" :
         r = ""
     else :
@@ -170,55 +213,76 @@ def int_minutes(arg): #convertit 1 entier donné (en minutes) en durée
 def get_relationship(obj,parcours): 
     return obj.is_relationship(parcours) 
  
-@register.simple_tag ##  
+@register.simple_tag  
 def get_used_in_parcours(obj,teacher): 
     return obj.used_in_parcours(teacher) 
 
  
-@register.simple_tag ##  résultats d'un savoir faire par élève
+@register.simple_tag  
 def get_scorek(obj,student): 
+    """
+    résultats d'un savoir faire par élève
+    """
     return obj.send_scorek(student)
 
 
-@register.simple_tag ##  résultat d'un savoir faire
+@register.simple_tag  
 def get_scorek2(results, id):
+    """
+    résultat d'un savoir faire
+    """
     return results.get(id, '')
 
 
 
 @register.simple_tag  
-def get_exercises_by_knowledge(obj,student,group): ##  
+def get_exercises_by_knowledge(obj,student,group):  
     return obj.exercices_by_knowledge(student,group)
 
 
 
-@register.simple_tag ##  teste si un exerice est utilisé dans un groupe
+@register.simple_tag 
 def get_exercise_used(obj,parcours_tab): 
+    """
+    teste si un exerice est utilisé dans un groupe
+    """
     return obj.nb_exercise_used(parcours_tab)
 
 
-@register.simple_tag  ##  retourne le score par thème d'un groupe par élève
+@register.simple_tag  
 def get_score_by_theme(obj,student,group): 
+    """
+    retourne le score par thème d'un groupe par élève
+    """
     return obj.as_score_by_theme(student,group)
 
 
-@register.simple_tag ##  retourne le score moyen des exercices par élève d'un groupe
+@register.simple_tag
 def get_knowledge_average(obj,group): 
+    """
+    retourne le score moyen des exercices par élève d'un groupe
+    """
     return obj.knowledge_average(group)
 
 
-@register.simple_tag ##  
+@register.simple_tag
 def get_knowledge_worked(obj,student): 
     return obj.nb_knowledge_worked(student)
 
 
-@register.simple_tag ##  retroune le nombre de taches données par groupe
+@register.simple_tag
 def get_nb_task_done(obj,group): 
+    """
+    retourne le nombre de taches données par groupe
+    """
     return obj.nb_task_done(group)
 
 
-@register.simple_tag ## retourne une liste des élèves du groupes qui ont fait l'exercice.
+@register.simple_tag
 def get_who_are_done(obj,group): 
+    """
+    retourne une liste des élèves du groupes qui ont fait l'exercice.
+    """
     return obj.who_are_done(group)
 
 
@@ -227,66 +291,88 @@ def get_nb_task_parcours_done(obj,parcours):
     return obj.nb_task_parcours_done(parcours)
 
 
-@register.simple_tag ## retourne une liste des élèves du parcours qui ont fait l'exercice.
+@register.simple_tag
 def get_who_are_done_parcours(obj,parcours): 
+    """
+    retourne une liste des élèves du parcours qui ont fait l'exercice.
+    """
     return obj.who_are_done_parcours(parcours)
 
 
 
-@register.simple_tag ## booleen si l'élève appartient ou pas à un parcours
+@register.simple_tag 
 def get_exercise(obj, exercise): 
+    """
+    booleen si l'élève appartient ou pas à un parcours
+    """
     return obj.has_exercise( exercise)
 
-@register.simple_tag ## retourne le nombre d'exercices par parcours publiés.
+@register.simple_tag 
 def get_all_details(obj,parcours): 
+    """
+    retourne le nombre d'exercices par parcours publiés.
+    """
     return obj.all_details(parcours)
 
 
 
 
-@register.simple_tag ## booleen si l'élève appartient ou pas à un parcours
-def get_in_parcours(obj, parcours): 
+@register.simple_tag  
+def get_in_parcours(obj, parcours):
+    """
+    booleen si l'élève appartient ou pas à un parcours
+    """ 
     return obj.is_in_parcours( parcours)
 
 
  
-@register.simple_tag ## donne le score par knowledge 
+@register.simple_tag 
 def get_score_student_parcours(obj,student, parcours): 
     return obj.score_student_parcours(student, parcours)
 
  
-@register.simple_tag ## donne le score par knowledge 
+@register.simple_tag 
 def get_score_student_parcours(obj,student, parcours): 
     return obj.score_student_parcours(student, parcours)
 
 
-@register.simple_tag ## donne le score par knowledge 
+@register.simple_tag  
 def get_percent_student_done_parcours_exercice(obj, parcours,group_id): 
+
     return obj.percent_student_done_parcours_exercice(parcours,group_id)
 
 
 
 
-@register.simple_tag ## donne le score par relation 
+@register.simple_tag 
 def get_score_student_for_this(obj, student): 
+    """
+    renvoie le score par relation pour un étudiant donné
+    """
     return obj.score_student_for_this(student)
 
 
 
-@register.simple_tag ## teste si la relationship est sous constrainte pour cet élève
+@register.simple_tag
 def get_constraint_to_this_relationship(obj, student): 
+    """
+    teste si la relationship est sous constrainte pour cet élève
+    """
     return obj.constraint_to_this_relationship(student)
 
 
 
-@register.simple_tag ## donne le score par relation
+@register.simple_tag 
 def get_an_association_knowledge_supportfile(obj, supportfile): 
+    """
+    renvoie le score par relation
+    """
     return obj.association_knowledge_supportfile(supportfile)
 
 @register.simple_tag 
 def get_parcours_group_students_count(obj, group):
     """
-    renvoie le score par relation
+    renvoie le nombre de parcours
     """ 
     return obj.parcours_group_students_count(group)
 
