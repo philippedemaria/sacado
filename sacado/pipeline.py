407554c49f881798af3ec2bbd0e2549a4ff3bbbc
Raphael Chay <raphael.chay@gmail.com>
2020-07-13 11:44:32 -0500

    choix profile

--- sacado/pipeline.py
+++ sacado/pipeline.py
@@ -1,13 +1,21 @@
 from django.shortcuts import redirect
 from social_core.pipeline.partial import partial
 
-from account.models import Student
+from account.models import User, Student, Parent, Teacher
 
 
 def complete_user(**kwargs):
-    import pdb;pdb.set_trace()
+
     user = kwargs['user']
-    Student.objects.create(user_id=user.pk, level_id=1)
+    usertype = int(kwargs['details']['usertype'])
+    import pdb
+    pdb.set_trace()
+    if usertype == User.STUDENT:
+        Student.objects.create(user_id=user.pk, level_id=1)
+    elif usertype == User.PARENT:
+        Parent.objects.create(user_id=user.pk)
+    elif usertype == User.TEACHER:
+        Teacher.objects.create(user_id=user.pk)
     return kwargs
 
 
@@ -16,9 +24,13 @@ def complete_user(**kwargs):
 
 @partial
 def get_usertype(strategy, details, user=None, is_new=False, *args, **kwargs):
-    email = strategy.request_data().get('email')
-    if email:
-        details['email'] = email
-    else:
-        return redirect('ask_usertype')
+
+    if user:
+        return
+    elif is_new and not details.get('usertype'):
+        usertype = strategy.request_data().get('usertype')
+        if usertype:
+            details['usertype'] = usertype
+        else:
+            return redirect('ask_usertype')
 
