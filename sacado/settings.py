14a3c7140240e3a0a08f061be17411b7f3329527
Philippe Demaria <philippe.demaria83@gmail.com>
2020-09-29 09:21:15 +0100

    Update settings.py

--- sacado/settings.py
+++ sacado/settings.py
@@ -42,7 +42,7 @@ if PRODUCTION:
     CONN_MAX_AGE = os.environ.get('CONN_MAX_AGE')
 
 else:
-    # configuration développement
+    # configuration dévellopement
     DEBUG = True
     ALLOWED_HOSTS = ["*"]
     SECRET_KEY = '85umr_$zf2bd58xl)nzf)i*jh)o5h*dp%*3e@pqg+ijem=t1xq'
@@ -295,25 +295,25 @@ CKEDITOR_CONFIGS = {
  
 
 ########################################################################################################################
-#if PRODUCTION :
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'file': {
-            'level': 'DEBUG',
-            'class': 'logging.handlers.RotatingFileHandler',
-            'filename': 'logs/debug.log',
-            'backupCount': 10,  # keep at most 10 log files
-            'maxBytes': 1048576,  # 1*1024*1024 bytes (1MB)
-        },
+if PRODUCTION :
+    LOGGING = {
+        'version': 1,
+        'disable_existing_loggers': False,
+        'handlers': {
+            'file': {
+                'level': 'DEBUG',
+                'class': 'logging.handlers.RotatingFileHandler',
+                'filename': 'logs/debug.log',
+                'backupCount': 10,  # keep at most 10 log files
+                'maxBytes': 1048576,  # 1*1024*1024 bytes (1MB)
+            },
 
-    },
-    'loggers': {
-        'django': {
-            'handlers': ['file'],
-            'level': 'DEBUG',
-            'propagate': True,
         },
-    },
-}
+        'loggers': {
+            'django': {
+                'handlers': ['file'],
+                'level': 'DEBUG',
+                'propagate': True,
+            },
+        },
+    }
