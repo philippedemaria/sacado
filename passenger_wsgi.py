5889228117865b7915354f3b93d0af49b4ee4c43
Philippe Demaria <philippe.demaria83@gmail.com>
2020-10-15 18:37:48 +0100

    passenger_wsgi

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -11,12 +11,16 @@ class PassengerPathInfoFix(object):
     def __call__(self, environ, start_response):
         from urllib.parse import unquote
         environ['SCRIPT_NAME'] = SCRIPT_NAME
+        try :
+            request_uri = unquote(environ['REQUEST_URI'])
+            script_name = unquote(environ.get('SCRIPT_NAME', ''))
+            offset = request_uri.startswith(script_name) and len(environ['SCRIPT_NAME']) or 0
+            environ['PATH_INFO'] = request_uri[offset:].split('?', 1)[0]
+            return self.app(environ, start_response)
+        except :
+            return self.app(environ, start_response)
 
-        request_uri = unquote(environ['REQUEST_URI'])
-        script_name = unquote(environ.get('SCRIPT_NAME', ''))
-        offset = request_uri.startswith(script_name) and len(environ['SCRIPT_NAME']) or 0
-        environ['PATH_INFO'] = request_uri[offset:].split('?', 1)[0]
-        return self.app(environ, start_response)
+
 
 application = sacado.wsgi.application
 application = PassengerPathInfoFix(application)
