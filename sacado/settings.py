0b50edc8f582c8cb8ccb546fc5dfb87bfccbab92
raphael chay <raphael.chay@gmail.com>
2021-02-01 07:20:56 -0500

    sentry + requirements

--- sacado/settings.py
+++ sacado/settings.py
@@ -2,7 +2,9 @@ import os
 from PIL import Image
 from django.urls import reverse_lazy
 from django.conf import global_settings
- 
+
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
