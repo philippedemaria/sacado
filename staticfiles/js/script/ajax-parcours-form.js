6b46f931b7eb61b0e1d2fd229eab96720716408f
Philippe Demaria <philippe.demaria83@gmail.com>
2021-03-23 17:26:52 +0100

    select student all_students

--- staticfiles/js/script/ajax-parcours-form.js
+++ staticfiles/js/script/ajax-parcours-form.js
@@ -1,11 +1,23 @@
 define(['jquery','bootstrap'], function ($) {
     $(document).ready(function () {
-        console.log("chargement JS ajax-evaluation.js OK");
+        console.log("chargement JS ajax-parcours-complement.js OK");
  
+
+        $("#id_is_publish").on('change', function (event) {
+            $('.publication_div').toggle(500);
+        });
 
 
         $('[type=checkbox]').prop('checked', false);        
-         $('#id_is_favorite').prop('checked', true); 
+        $('#id_is_favorite').prop('checked', true); 
+
+
+        // Pour le form_parcours
+ 
+         $('#folder_div').hide(); 
+
+
+         $('#leaf_div').hide(); 
 
 
 
@@ -19,13 +31,6 @@ define(['jquery','bootstrap'], function ($) {
             value = $(this).val(); 
             $('#demosg'+value+' .selector').not(this).prop('checked', this.checked);
         });
-
-
-
-
-
-
-
-
+        
     });
 });
