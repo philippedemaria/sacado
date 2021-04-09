37e6ad08a8021789ee73b48eff86720eeafcd5ef
Philippe Demaria <philippe.demaria83@gmail.com>
2021-04-09 14:54:05 +0100

    Update models.py

--- qcm/models.py
+++ qcm/models.py
@@ -162,7 +162,9 @@ class Exercise(models.Model):
                                   verbose_name="Savoir faire associé - Titre")
     supportfile = models.ForeignKey(Supportfile, blank=True, default=1, related_name="exercises",
                                     on_delete=models.PROTECT, verbose_name="Fichier Géogebra")
-  
+    #ranking = models.PositiveIntegerField(  default=0,  blank=True, null=True, editable=False)
+
+
 
     def __str__(self):
         return "{}".format(self.knowledge.name)
