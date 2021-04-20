e773282c9c54d6551e9e9014f31b5901c4d55033
Philippe Demaria <philippe.demaria83@gmail.com>
2021-04-20 16:34:08 +0100

    update

--- payment_fonctions.py
+++ payment_fonctions.py
@@ -3,13 +3,13 @@ from datetime import  datetime
 
 def accounting_adhesion(school, today , acting, user, is_active , observation ): #acting = date d'effet
 
-    fee = school.fee()
+    fee       = school.fee()
     today     = datetime.now()
     this_year = today.year
     next_year = int(this_year) + 1
-    objet = "Cotisation "+ str(this_year) +" - " +str(next_year)
+    objet     = "Cotisation "+ str(this_year) +" - " +str(next_year)
 
-    accounting, create = Accounting.objects.get_or_create( school = school   , is_active = 0 ,  defaults={ 'date' : today , 'amount' : fee , 'objet' : objet , 'beneficiaire' :  "SACADO ASSO" ,
+    accounting, create = Accounting.objects.get_or_create( school = school   , is_active = 0 ,  defaults={ 'date' : today , 'amount' : fee , 'objet' : objet , 'beneficiaire' :  school.name ,
                                                                                                                     'is_credit' : 1, 'address' :  school.address , 'complement' : school.complement ,
                                                                                                                     'town' : school.town , 'country' : school.country , 'contact' : school.address ,
                                                                                                                      'observation' : observation ,'acting' : acting , 'user' : user  })
