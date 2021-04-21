8a1f6d4e0eb1c7509cdc09291fd85c30b7b9b6c3
Philippe Demaria <philippe.demaria83@gmail.com>
2021-04-21 16:00:09 +0100

    Update urls.py

--- sacado/urls.py
+++ sacado/urls.py
@@ -17,7 +17,7 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import  path, include, re_path
 from django.conf.urls.static import static
-import cas.views
+#import cas.views
 
 urlpatterns = [
                   path('', include('setup.urls')),
