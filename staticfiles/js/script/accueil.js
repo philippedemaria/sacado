ae87ca362fdf13d80c70976d79ff9cb6ef7af24b
Philippe Demaria <philippe.demaria83@gmail.com>
2021-09-04 18:49:52 +0100

    home page

--- staticfiles/js/script/accueil.js
+++ staticfiles/js/script/accueil.js
@@ -495,8 +495,8 @@ $(document).ready(function () {
 
 
       $(".regular").slick({
-            slidesToShow: 2,
-            slidesToScroll: 2,
+            slidesToShow: 3,
+            slidesToScroll: 3,
             autoplay: false,
             autoplaySpeed: 2000,        
             dots: false,
