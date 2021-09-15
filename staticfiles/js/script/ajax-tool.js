5abc2bc6c7bafb6eefeb48192004938fe1926a99
Philippe Demaria <philippe.demaria83@gmail.com>
2021-09-15 19:24:33 +0100

    Update ajax-tool.js

--- staticfiles/js/script/ajax-tool.js
+++ staticfiles/js/script/ajax-tool.js
@@ -39,8 +39,8 @@ define(['jquery',  'bootstrap'], function ($) {
 
             if($('iframe').length) { 
 
-                width = 1.5*parseInt($('body').find("iframe").attr("width"));
-                height = 1.5*parseInt($('body').find("iframe").attr("height")); 
+                width = 1.2*parseInt($('body').find("iframe").attr("width"));
+                height = 1.2*parseInt($('body').find("iframe").attr("height")); 
                 coeff = width/height                                    
 
                 if (width < screen_size){
