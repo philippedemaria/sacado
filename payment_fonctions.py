aef8062338e7054b0a04746e270230296839ccfa
Philippe Demaria <philippe.demaria83@gmail.com>
2021-09-23 21:01:30 +0100

    Update payment_fonctions.py

--- payment_fonctions.py
+++ payment_fonctions.py
@@ -13,7 +13,7 @@ def accounting_adhesion(school, today , date_payment, user, is_active , observat
 
     accounting, create = Accounting.objects.get_or_create( school = school   , is_active = 0 ,  defaults={ 'date' : today , 'amount' : fee , 'objet' : objet , 'beneficiaire' :  school.name , 'chrono' : create_chrono(Accounting,"FACTURE") ,
                                                                                                                     'is_credit' : 1 , 'address' :  school.address , 'complement' : school.complement , 'is_abonnement' : True ,
-                                                                                                                    'town' : school.town , 'country' : school.country , 'contact' : school.address , 'forme' : "FACTURE",
+                                                                                                                    'town' : school.town , 'country' : school.country , 'contact' : school.address , 'forme' : "FACTURE", 'plan_id' :  18 ,
                                                                                                                      'observation' : observation ,'date_payment' : date_payment , 'user' : user  })
     if create :
 
