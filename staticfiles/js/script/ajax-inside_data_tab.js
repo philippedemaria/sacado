8c8b04116838daae5797c3e155e45a3c6b91117a
Philippe Demaria <philippe.demaria83@gmail.com>
2021-09-29 22:23:41 +0100

    Update ajax-inside_data_tab.js

--- staticfiles/js/script/ajax-inside_data_tab.js
+++ staticfiles/js/script/ajax-inside_data_tab.js
@@ -10,9 +10,9 @@ define(['jquery','bootstrap_popover', 'bootstrap' ], function ($) {
 
         $('.dataTables_length').append("  <a href='#' data-toggle='modal' data-target='#export_results'  class='btn btn-default btn-publisher  pull-right'><i class='fa fa-print'></i></a>  ") ;        
 
-        $('.dataTables_length').append("  <a href='#' data-toggle='modal' data-target='#export_marks'  class='btn btn-default pull-right'>Exporter les notes</a>  ") ;
+        $('.dataTables_length').append("  <a href='#' data-toggle='modal' data-target='#export_marks'  class='btn btn-default pull-right'>Exporter les notes CSV</a>  ") ;
 
-        $('.dataTables_length').append("  <a href='#' data-toggle='modal' data-target='#export_skills'  class='btn btn-default pull-right'>Exporter les compétences</a>  ") ;
+        $('.dataTables_length').append("  <a href='#' data-toggle='modal' data-target='#export_skills'  class='btn btn-default pull-right'>Exporter les compétences CSV</a>  ") ;
 
 
 
