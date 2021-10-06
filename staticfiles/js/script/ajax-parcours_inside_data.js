10a8b929421570fc1233111f2f1bc008fe48d1f3
Philippe Demaria <philippe.demaria83@gmail.com>
2021-10-07 00:42:53 +0100

    update

--- staticfiles/js/script/ajax-parcours_inside_data.js
+++ staticfiles/js/script/ajax-parcours_inside_data.js
@@ -2,7 +2,7 @@ define(['jquery','bootstrap_popover', 'bootstrap' ], function ($) {
     $(document).ready(function () {
         console.log("chargement JS inside_data_tab.js OK");
 
-        $('#DataTables_Table_1_wrapper').find('.col-sm-6').first().append("<h1 class='thin sacado_color_text'><i class='fa fa-folder-open'></i> Mes dossiers </h1> ") ;
-        $('#DataTables_Table_0_wrapper').find('.col-sm-6').first().append("<h1 class='thin sacado_color_text'><i class='fa fa-th'></i> Mes parcours </h1> ") ;
+        $('#DataTables_Table_0_wrapper').find('.col-sm-6').first().append("<h1 class='thin sacado_color_text'><i class='fa fa-folder-open'></i> Dossiers </h1> ") ;
+        $('#DataTables_Table_1_wrapper').find('.col-sm-6').first().append("<h1 class='thin sacado_color_text'><i class='fa fa-th'></i> Parcours </h1> ") ;
     });
 });
