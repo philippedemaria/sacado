c24e79a843149a4a619ff45a85c9c8d4d7a7b11a
Philippe Demaria <philippe.demaria83@gmail.com>
2021-11-04 09:49:24 +0100

    Update views.py

--- setup/views.py
+++ setup/views.py
@@ -282,7 +282,7 @@ def ressource_sacado(request): #Protection saml pour le GAR
             request.session["user_id"] = request.user.id
             return redirect('dashboard')
         else :
-            messages.error(request,"Erreur de connexion à SACADO : user = ", username, " password = ",password)
+            messages.error(request,"Erreur de connexion à SACADO : user = {} et  password = {}".format(username, password) )
             return redirect('index')
 
     else :
