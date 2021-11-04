46088213f01701a7d38cc7699b49370296e463b0
Philippe Demaria <philippe.demaria83@gmail.com>
2021-11-04 11:27:08 +0100

    Update views.py

--- setup/views.py
+++ setup/views.py
@@ -276,7 +276,7 @@ def ressource_sacado(request): #Protection saml pour le GAR
             teacher,created_s = Teacher.objects.get_or_create(user = user, defaults = { "notification" : 0 , "exercise_post" : 0    })
 
  
-        user_connected = authenticate( username=user.username, password="sacado_gar")
+        user_connected = authenticate( username=user.username, password="sacado_gars")
 
         if user_connected is not None:
             login(request, user_connected,  backend='django.contrib.auth.backends.ModelBackend' )
