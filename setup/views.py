6445c7057053f21d6c5eb3088b699d7a27469dcf
Philippe Demaria <philippe.demaria83@gmail.com>
2021-11-04 15:41:49 +0100

    Update views.py

--- setup/views.py
+++ setup/views.py
@@ -272,6 +272,8 @@ def ressource_sacado(request): #Protection saml pour le GAR
         elif user_type == 2 and created :
             teacher,created_s = Teacher.objects.get_or_create(user = user, defaults = { "notification" : 0 , "exercise_post" : 0    })
 
+        texte = "coucou"
+        messages.error(request, texte  )
         user_authenticated = authenticate( username= username, password= "sacado_gar")
 
         if user_authenticated is not None:
@@ -279,7 +281,7 @@ def ressource_sacado(request): #Protection saml pour le GAR
             request.session["user_id"] = user.id
             chn = "voici"
             if user_authenticated.is_authenticated :
-                chn = "voilà"
+                chn = "voilà"  
             messages.error(request, chn  )
 
         else : 
