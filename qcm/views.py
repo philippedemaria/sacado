4876fbab1493fce40a1e2f556f0506ae526649d4
Philippe Demaria <philippe.demaria83@gmail.com>
2021-11-10 22:35:52 +0100

    maj

--- qcm/views.py
+++ qcm/views.py
@@ -1572,7 +1572,11 @@ def list_sub_parcours_group_student(request,idg,idf):
     parcourses = bases.filter( is_evaluation=0).order_by("ranking")
     evaluations = bases.filter( is_evaluation=1).order_by("ranking")
 
-    context = {'parcourses': parcourses , 'evaluations': evaluations , 'student' : student , 'group' : group ,  'folder' : folder,    'today' : today }
+
+    quizzes = folder.quizz.filter(Q(is_publish=1) | Q(start__lte=today, stop__gte=today),  is_archive=0 ) 
+    bibliotexs = folder.bibliotexs.filter(Q(is_publish=1) | Q(start__lte=today, stop__gte=today), students = student )
+
+    context = {'parcourses': parcourses , 'evaluations': evaluations , 'bibliotexs': bibliotexs ,  'quizzes': quizzes ,   'student' : student , 'group' : group ,  'folder' : folder,    'today' : today }
 
     return render(request, 'qcm/list_sub_parcours_group_student.html', context )
 
