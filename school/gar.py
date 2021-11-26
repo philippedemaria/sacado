c320835c8933e0599b2f83930c95c0fc7548b2c7
Philippe Demaria <philippe.demaria83@gmail.com>
2021-11-26 06:07:04 +0100

    Update gar.py

--- school/gar.py
+++ school/gar.py
@@ -7,10 +7,16 @@ import requests
 def date_abonnement(today):
     """Création d'un abonnement dans la base de données"""
     date_start = today.isoformat() # Année en cours
+    date_start = date_start.split("T")
+    date_start = date_start[0]
     if today < datetime( today.year,5,30) :
         date_stop  = datetime(today.year,7,14).isoformat() # Année en cours
     else :
         date_stop  = datetime(today.year+1,7,14).isoformat() # Année suivante
+
+    date_stop = date_stop.split("T")
+    date_stop = date_stop[0]
+
 
     return date_start, date_stop
 
@@ -20,7 +26,7 @@ def web_abonnement_xml(abonnement,id_abonnement , today):
     #Webservice du GAR
 
     date_start, date_stop = date_abonnement(today)
-
+ 
     body = "<idAbonnement>" + id_abonnement +"</idAbonnement>"
     body += "<commentaireAbonnement>AbonnementSacAdo</commentaireAbonnement>"
     body += "<idDistributeurCom>832020065_000000000000000</idDistributeurCom>"
@@ -30,18 +36,20 @@ def web_abonnement_xml(abonnement,id_abonnement , today):
     body += "<debutValidite>"+date_start+"</debutValidite>"
     body += "<finValidite>"+date_stop+"</finValidite>"
     body += "<uaiEtab>"+abonnement.school.code_acad+"</uaiEtab>"
-    body += "<categorieAffectation>transferable</categorieAffectation>"
+    body += "<categorieAffectation>TRANSFERABLE</categorieAffectation>"
     body += "<typeAffectation>INDIV</typeAffectation>"
     body += "<nbLicenceEnseignant>500</nbLicenceEnseignant>"
     body += "<nbLicenceEleve>"+str(abonnement.school.nbstudents)+"</nbLicenceEleve>"
     body += "<nbLicenceProfDoc>100</nbLicenceProfDoc>"
     body += "<publicCible>ENSEIGNANT</publicCible>"
     body += "<publicCible>ELEVE</publicCible>"
+    body += "<publicCible>DOCUMENTALISTES</publicCible>"
+    body += "<publicCible>AUTRES PUBLICS</publicCible>"
     body += "</abonnement>"
 
     return body
 
-
+ 
 
 def create_abonnement_gar(today,school,abonnement ,user):
     """Création d'un abonnement dans la base de données"""
