73e747aba0512b2764444227ec764575a7dd55bb
Philippe Demaria <philippe.demaria83@gmail.com>
2021-12-04 13:15:55 +0100

    MAJ CSS

--- staticfiles/js/script/ajax-flashcard.js
+++ staticfiles/js/script/ajax-flashcard.js
@@ -3,36 +3,31 @@ define(['jquery', 'bootstrap'], function ($) {
         console.log("chargement JS ajax-flashcard.js OK");
 
   
-
+        $("#this_question_textarea_display").click(function(){ 
+            var value = CKEDITOR.instances['id_question'].getData();
+            $('#type_of_textarea_display').html("la question");
+            $('#body_of_textarea_display').html(value);
+            $('#body_of_textarea_display').addClass("qflashcard").removeClass("hflashcard").removeClass("aflashcard");
+        });
 
 
         $("#this_answer_textarea_display").click(function(){ 
             var value = CKEDITOR.instances['id_answer'].getData();
             $('#type_of_textarea_display').html("la réponse");
             $('#body_of_textarea_display').html(value);
-            
+            $('#body_of_textarea_display').addClass("aflashcard").removeClass("hflashcard").removeClass("qflashcard");
         });
 
-         
-        $("#this_question_textarea_display").click(function(){ 
-            var value = CKEDITOR.instances['id_question'].getData();
-            $('#type_of_textarea_display').html("la question");
-            $('#body_of_textarea_display').html(value);
-            
-        });
-
-
- 
+        
         $("#this_helper_textarea_display").click(function(){ 
             var value = CKEDITOR.instances['id_helper'].getData();
             $('#type_of_textarea_display').html("l'aide");
             $('#body_of_textarea_display').html(value);
-            
+            $('#body_of_textarea_display').addClass("hflashcard").removeClass("qflashcard").removeClass("aflashcard");
         });
 
 
-
-
+ 
 
 });
 
