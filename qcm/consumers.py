2b2e0f21e06a80533520df4990fc782a854a685a
Philippe Demaria <philippe.demaria83@gmail.com>
2021-12-04 21:48:01 +0100

    real-time ok

--- qcm/consumers.py
+++ qcm/consumers.py
@@ -3,153 +3,174 @@ from channels.generic.websocket import WebsocketConsumer , AsyncJsonWebsocketCon
 import json
 from channels.db import database_sync_to_async
 
-
+def printc(*a) :
+    pass
+#printc=print
 class TableauConsumer(AsyncJsonWebsocketConsumer):
 
     async def connect(self):
         """
         Called when the websocket is handshaking as part of initial connection.
         """
-        print("Tentative de connection, user=",self.scope["user"])
+        printc("Tentative de connection, user=",self.scope["user"])
         if self.scope["user"].is_anonymous:
-            print("utilisateur anonyme, on le deconnecte")
+            printc("utilisateur anonyme, on le deconnecte")
             await self.close()
         else :    
             await self.accept()
-            print("L'utilisateur est loggé, on poursuit...")
+            printc("L'utilisateur est loggé, on poursuit...")
             
          # Store which rooms the user has joined on this connection
             user=self.scope["user"]
-            print("type utilisateur :", user.user_type)
+            printc("type utilisateur :", user.user_type)
             if user.is_student :
-               print("c'est un élève")
-               print("nom de son channel :",self.channel_name)
+               printc("c'est un élève")
+               printc("nom de son channel :",self.channel_name)
 
             if user.is_teacher :
-               print("c'est un enseignant")
-               print("nom du channel prof :",self.channel_name)
+               printc("c'est un enseignant")
+               printc("nom du channel prof :",self.channel_name)
+               printc("Le prof a ouvert la page RT du parcours, c'est ",self.scope['user'])
 
-               
 
     async def disconnect(self, close_code):
-        print(self.scope['user'], " se deconnecte")
+        printc(self.scope['user'], " se deconnecte")
         # Leave room group
         self.channel_layer.group_discard(self.channel_name)
 
     async def receive_json(self,content) :
         command=content.get("command",None)
-        print("commande recue :", command)
+        printc("commande recue :", command)
         if command=="teacher_join" :
-               #-------création de la salle de realtime
-            print("Le prof a ouvert la page RT du parcours, c'est ",self.scope['user'])
-			   #------on ouvre le groupe       
-            print("le prof a initié le real time", self.scope['user'])
-            # on stockera dans la variable de session le groupe d'élèves
-            self.connected_students=dict()
-            self.role=2
-            us=self.scope["user"].id
-            print("user=",us)
-            
-            ugroupe="Salle"+str(content.get("parcours"))
-            self.ugroupe=ugroupe
-            print("nom du layer de tout le groupe : '{}'".format(ugroupe))
-            await self.channel_layer.group_add(ugroupe,self.channel_name)
-            print("ajout du groupe ok")
-            
-            await self.send_json({"type": "autojoin","salle": ugroupe})
-            print("renvoie de ok au prof : ok")
+               self.connected_students=dict()
+               self.role=2
+               us=self.scope["user"].id 
+               ugroupe="Salle"+str(content.get("parcours"))
+               self.ugroupe=ugroupe
+               
+               printc("nom du layer de tout le groupe : '{}'".format(ugroupe))
+               await self.channel_layer.group_add(ugroupe,self.channel_name)
+               printc("ajout du groupe ok")
+               printc("creation du groupe de contenant que le prof")
+               await self.channel_layer.group_add("perso"+ugroupe, self.channel_name)
+               await self.send_json({"type": "autojoin","salle": ugroupe})
+               printc("renvoie de ok au prof : ok")
         if command=="student_join" :
-            print("l'élève a ouvert un exo")
-            print("parcours : ", content.get("parcours"))
+            printc("l'élève a ouvert un exo")
+            printc("parcours : ", content.get("parcours"))
+            printc("exo",content.get("exo",None))
             us=self.scope["user"].id
             self.role=0 
-            print("id de l'élève :",us) 
+            printc("id de l'élève :",us) 
             ugroupe="Salle"+str(content.get("parcours"))
             self.ugroupe=ugroupe
-            print("champ user recu ",content.get("user"))
-            print("il est ajouté au layer de tout le groupe, qui se nomme '{}'".format(ugroupe))
+            printc("champ user recu ",content.get("user"))
+            printc("il est ajouté au layer de tout le groupe, qui se nomme '{}'".format(ugroupe))
             await self.channel_layer.group_add(ugroupe,self.channel_name)
-            uperso=ugroupe+'perso'+str(us)
-            #print("il est aussi ajouté à un layer contenant juste lui et le prof, qui se nomme",uperso)
-            #await self.channel_layer.group_add(uperso,self.channel_name)
-     
-            #print("on envoie les infos au layer général pour que le prof soit informé")
-            await self.channel_layer.group_send(ugroupe, 
+            await self.channel_layer.group_send("perso"+ugroupe, 
                 {'type' : "connexion.eleve",
                  'user' : self.scope["user"],
                  'channel' : self.channel_name})
-            print("envoi ok")
+            printc("envoi ok")
         if command=="student_message"  :
             message=content.get("message",None)
-            print("on va déclencher l'évènement message eleve au groupe '{}'".format(self.ugroupe))
-            print("message : '{}'".format(message))
-            await self.channel_layer.group_send(self.ugroupe,
+            printc("on va déclencher l'évènement message eleve au groupe '{}'".format("perso"+self.ugroupe))
+            printc("message : '{}'".format(message))
+            await self.channel_layer.group_send("perso"+self.ugroupe,
                {'type' : "student.message",
-                'from' : self.scope['user'].id, #expéditeur
-                'message' : message,
-                'name' : self.scope['user'].first_name +" "+  self.scope['user'].last_name ,
+                'from' : self.scope['user'].id, #expéditeur id
+                'name'  : self.scope['user'].username,
+                'message' : message})
+            printc("évènement student_message déclenché au groupe-singleton")
+        if command=="ExoDebut" :
+           await self.channel_layer.group_send("perso"+self.ugroupe,
+                {'type' : "ExoDebut",
+                 'from' : self.scope['user'].id, #expéditeur id
+                'name'  : self.scope['user'].username,
+                'ide' : content.get("ide"),  #identifiant d'exo
                 })
-            print("évènement student_message déclenché à tout le groupe")
+        if command=="SituationFinie" :
+           await self.channel_layer.group_send("perso"+self.ugroupe,
+                {'type' : "SituationFinie",
+                 'from' : self.scope['user'].id, #expéditeur id
+                'name'  : self.scope['user'].username,
+                'resultat' : content.get("resultat"),
+                'ide' : content.get("ide"),  #identifiant d'exo
+                "grade" : content.get("grade"),
+                "situation" : content.get("situation"),
+                "numexo"    : content.get("numexo")
+                
+               })
         if command=="teacher_message" :
-            print("command teacher_message recue" ,content)
+            printc("command teacher_message recue" ,content)
             message=content.get("message",None)
-            
-            #print("on va déclencher l'évènement teacher_message à tout le groupe '{}'".format(self.ugroupe))
-            print("message : '{}'".format(message))
-            await self.channel_layer.group_send(self.ugroupe,
+            to=content.get('to')
+            try :
+                chan_to=self.connected_students[int(to)][0]
+                
+            except : 
+                printc("eleve non trouvé ", self.connected_students)
+                chan_to="toto"
+            printc("message : '{}', to : {} channel : {}".format(message,to,chan_to))
+            await self.channel_layer.send(chan_to,
                {'type' : "teacher.message",
                 'to' : content.get('to',None),   #le destinataire à été envoyé par le client-prof
                 'message' : message})
-            print("évènement déclenché à tout le groupe")
+            printc("évènement déclenché au seul eleve destinataire")
         if command=="teacher_message_general" :
-             print("message general du prof")
-             message=content.get("message",None)
-             print("message ", message)
-             print("ugroupe ", self.ugroupe)
-             await self.channel_layer.group_send(self.ugroupe,
-                {'type' : "teacher.message.general",
-                 'message' : message})
-             print("evenement declenché pour tout le groupe")	
-
-
-
-
+            print("message general du prof")
+            message=content.get("message",None)
+            print("message ", message)
+            await self.channel_layer.group_send(self.ugroupe,
+               {'type' : "teacher.message.general",
+                'message' : message})
+            printc("evenement declenché pour tout le groupe")
+                        
 
     async def student_message(self,data) : #message envoyé par l'élève au prof
-        print("entree dans la fonction student_message")
-        print('user : ', self.scope['user'], self.role)
+        printc("entree dans la fonction student_message")
+        printc('user : ', self.scope['user'], self.role)
         if self.role==2 :   #c'est le prof qui reçoit, il faut donc lui envoyer
-            print("Envoyé par l'élève {}".format(data["from"]))
-            print("message :", data['message'])
+            printc("Envoyé par l'élève {}".format(data["from"]))
+            printc("message :", data['message'])
             await self.send_json({'type':'message','from': str(data["from"]),"message" : str(data['message']) , 'name' : data["name"]  })
     
+    
+    async def ExoDebut(self,data) :  #l'eleve commence un exo, le consumer du prof averti le client du prof
+        #printc("entree ExoDebut")
+        if self.role==2 :
+            await self.send_json(data)
+    async def SituationFinie(self,data) :  #l'eleve a termine une situation, le consumer du prof averti le client du prof
+        #printc("entree dans SituationFinie")
+        if self.role==2 :
+            await self.send_json(data)
+
     async def teacher_message(self,data) : #message envoyé par le prof à un eleve
-        print("entree dans la fonction teacher_message")
-        print('user : ', self.scope['user'], self.scope['user'].id,self.role)
-        print("self", self.scope['user'].id, "de type", type(self.scope['user'].id))
-        print("destinataire", data['to'],type(data['to']))
+        printc("entree dans la fonction teacher_message")
+        printc('user : ', self.scope['user'], self.scope['user'].id,self.role)
+        printc("self", self.scope['user'].id, "de type", type(self.scope['user'].id))
+        printc("destinataire", data['to'],type(data['to']))
         if  self.scope['user'].id == data['to'] : #on est dans l'instance de l'élève destinataire
-            print("Envoyé à l'élève {}".format(data["to"]))
-            print("message :", data['message'])
+            printc("Envoyé à l'élève {}".format(data["to"]))
+            printc("message :", data['message'])
             await self.send_json({'type':'message','to': data["to"],"message" : data['message']})
-            print("ok")
+            printc("ok")
         else :
-            print("self n'est pas le destinataire du message, rien à faire")
+            printc("self n'est pas le destinataire du message, rien à faire")
 
     async def teacher_message_general(self,data) : #message envoyé par le prof à tous les eleves
-          print("entree dans teacher message general")
+          printc("entree dans teacher message general")
           await self.send_json({'type':'message','from': "prof", "name": "moi", "message" : str(data['message'])})
 
 
     async def connexion_eleve(self,data):
-            print("entree dans la fonction connexion eleve")
-            print("data=",data)
-            print("destinataire {} (role : {})".format(self.scope['user'], self.role))
+            printc("entree dans la fonction connexion eleve")
+            printc("data=",data)
+            printc("destinataire {} (role : {})".format(self.scope['user'], self.role))
             if self.role==2 :
-                print("destinataire : le prof")
+                printc("destinataire : le prof")
                 self.connected_students[data['user'].id]=(data['channel'],data['user'])
-                print("connected_students : ", self.connected_students)
+                printc("connected_students : ", self.connected_students)
                 await self.send_json({'type' : 'connexion' , 'from' : data["user"].id })
-
 
 
--- staticfiles/js/script/ajax-real-time-parcours.js
+++ staticfiles/js/script/ajax-real-time-parcours.js
@@ -26,13 +26,10 @@ define(['jquery',  'bootstrap', 'websocket' ], function ($) {
 
 
         function PostMessage(id){  //id = destinataire du message
-            console.log("entree dans postMesssage");
-            console.log( id ,  $("#champ"+id).val() );
             socket.send(JSON.stringify(
                {"command":"teacher_message",
                  "to" : id,
                 "message": $("#champ"+id).val() }));
-                console.log("message envoyé");
                  document.getElementById("champ"+id).value = ""; 
             };
 
@@ -40,17 +37,34 @@ define(['jquery',  'bootstrap', 'websocket' ], function ($) {
 
         $("body").on('change', "#entreechat", function (event) {
 
-            console.log("entree dans postMesssageGeneral");
             socket.send(JSON.stringify(
                 {"command":"teacher_message_general",
                  "message": $("#entreechat").val() }));
-                 console.log("message général envoyé"); 
                  document.getElementById("entreechat").value = "";          
              
         }) ;
 
 
-
+        function barreVierge(canvas){  //affichage d'une barre grise  
+            var largeur=80;
+            var hauteur=12;
+            ctx=canvas.getContext("2d");
+            ctx.fillStyle="#A0A0A0";
+            ctx.fillRect(0,0,largeur,hauteur);
+            ctx.stroke();
+          }
+          
+        function barrePrecis(canvas,a,ok,t){  //affichage d'un seul rectangle
+                                     
+            var largeur=80;
+            var hauteur=12;
+            //console.log("situation terminee "+ok+","+a+","+t);
+            if (t==0) {t=1;a=0;};
+            ctx=canvas.getContext("2d");
+            if (ok) {ctx.fillStyle="#00FF00";} else {ctx.fillStyle="#FF0000";}
+            ctx.fillRect(a*largeur/t,0,largeur/t,hauteur);
+            ctx.stroke();
+          }
  
     
  
@@ -58,7 +72,6 @@ define(['jquery',  'bootstrap', 'websocket' ], function ($) {
           // Correctly decide between ws:// and wss://
           var ws_scheme = window.location.protocol == "https:" ? "wss" : "ws";
           var ws_path = ws_scheme + '://' + window.location.host + "/qcm/tableau/";
-          console.log("tentative connexion to " + ws_path);
           window.socket = new WebSocket(ws_path); // window pour rendre globale la variable
           socket.onopen = function () {
               console.log("Connected to socket");
@@ -66,7 +79,6 @@ define(['jquery',  'bootstrap', 'websocket' ], function ($) {
               "command":"teacher_join", 
               "parcours": parcours_id ,
               "students":"{% for student in  students %}{{student.user.id}}|{% endfor %}"}));
-              console.log("commande teacher join envoyée");
           };
 
 
@@ -75,57 +87,55 @@ define(['jquery',  'bootstrap', 'websocket' ], function ($) {
  
 
                     // Handle incoming messages
-           socket.onmessage = function (message) {
-           // Decode the JSON
-           console.log("Got websocket message " + message.data);
-           var data = JSON.parse(message.data);
-           // Handle errors
-           if (data.error) {
-             alert(data.error);
-             return;
-             }
-
-            console.log(data) ; 
+            socket.onmessage = function (message) {
+                 // Decode the JSON
+                 var data = JSON.parse(message.data);
+                 // Handle errors
+                 if (data.error) {
+                   return;
+                   }
 
 
+                 if (data.type=="autojoin")
+                 // connexion du prof
+                  {
+                      console.log("connexion du prof ok");
+                  }
+                 else if (data.type=="connexion")
+                  { //console.log("connexion d'un eleve");
+                   ligne=document.getElementById("tr_student_"+data.from)
+                   $("#tr_student_"+data.from).find("td:first").addClass("live");
+                   if (ligne.childNodes[1].innerHTML.search("<p><input")==-1)
+                        {ligne.childNodes[1].innerHTML=ligne.childNodes[1].innerHTML+
+                   "<p><input type=\"text\" id='champ"+data.from+"' data-from_id="+data.from+
+                   " placeholder='message privé' required class='this_student_rt' /></p>";
+                       }
+                   //ligne[1].innerHTML=data.message;
+                  }
+                  else if (data.type=="message") {
+                    console.log(data.from+" a envoyé un message");
+                    var t=document.getElementById("chat");             
+                    t.innerHTML = t.innerHTML + "<p style='color:red'>"+ data.name+"</p><p style='text-align:right'>"+ data.message+"</p>";
+                            
+                    }// Deconnexion  
 
-           if (data.type=="autojoin")
-           // connexion du prof
-            {
-                console.log("connexion du prof ok");
-            }
-           else if (data.type=="connexion")
-            {console.log("connexion d'un eleve");
-             ligne=document.getElementById("tr_student_"+data.from)
-             console.log("type table : "+ligne.nodeName)
-             $("#tr_student_"+data.from).find("td:first").addClass("live");
-             ligne.childNodes[1].innerHTML=ligne.childNodes[1].innerHTML+
-             "<p><input type=\"text\" id='champ"+data.from+"' data-from_id="+data.from+" placeholder='message privé' required class='this_student_rt' /></p>";
-
-             //ligne[1].innerHTML=data.message;
-            }
-            else if (data.type=="message") {
-              console.log(data.from+" a envoyé un message");
-              var t=document.getElementById("chat");             
-              t.innerHTML = t.innerHTML + "<p style='color:red'>"+ data.name+"</p><p style='text-align:right'>"+ data.message+"</p>";
-                      
-              }// Deconnexion  
-            // else if (data.type=="déconnexion") {
-            //     console.log(data.from+" s'est déconnecté");
-                //$("#room-" + data.leave).remove();
-            //      var t=document.getElementById(data.de);
-            //      t.textContent=data.last_name+ "(Déconnecté)";
-            //     }
-            //    }
-
-
-            else if (data.type=="déconnexion") {
-                 console.log(data.from+" s'est déconnecté");
-              ligne=document.getElementById("tr_student_"+data.from);
-              console.log("type table : "+ligne.nodeName);
-              ligne.childNodes[1].innerHTML=data.name;
-                }
-        }
+                  else if (data.type=="déconnexion") {
+                       //console.log(data.from+" s'est déconnecté");
+                    ligne=document.getElementById("tr_student_"+data.from);
+                    //console.log("type table : "+ligne.nodeName);
+                    ligne.childNodes[1].innerHTML=data.name;
+                      }
+                  else if (data.type=="ExoDebut") {
+                        //console.log(data.from+" a initié l'exo " +data.ide);
+                        var canvas=document.getElementById(data.ide+"|"+data.from);
+                        barreVierge(canvas);
+                      }
+                  else if (data.type=="SituationFinie"){
+                      //console.log(data.from+" a termine une situation" +data.numexo);
+                      var canvas=document.getElementById(data.ide+"|"+data.from)
+                      barrePrecis(canvas,data.numexo,data.resultat,data.situation);
+                    }
+              }
 
 
         // Helpful debugging
