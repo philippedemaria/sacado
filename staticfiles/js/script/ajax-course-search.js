5df5fcabd076537d709a7b2cb06d2d25231605a5
Philippe Demaria <philippe.demaria83@gmail.com>
2022-01-15 15:21:41 +0100

    Update ajax-course-search.js

--- staticfiles/js/script/ajax-course-search.js
+++ staticfiles/js/script/ajax-course-search.js
@@ -186,6 +186,83 @@ define(['jquery', 'bootstrap'], function ($) {
 
 
 
+        // Affiche dans la modal le modèle pour récupérer un exercice custom
+        $('body').on('click', '.shower_course' , function (event) {
+            let course_id = $(this).attr("data-course_id");
+            let csrf_token = $("input[name='csrfmiddlewaretoken']").val();
+
+            $.ajax(
+                {
+                    type: "POST",
+                    dataType: "json",
+                    data: {
+                        'course_id': course_id,
+                        csrfmiddlewaretoken: csrf_token
+                    },
+                    url: "../parcours_shower_course",
+                    success: function (data) {
+
+                        $('#get_course_title').html(data.title);
+                        $('#get_course_body').html(data.html);
+                    }
+                }
+            )
+         });
+
+
+
+
+        // Affiche dans la modal le modèle pour récupérer un exercice custom
+        $('#get_course_result').on('click', ".clone_to" ,  function (event) {
+
+            let course_id = $(this).attr("data-course_id");
+            let csrf_token = $("input[name='csrfmiddlewaretoken']").val();
+            var checkbox_value = "";
+            let all_parcours = $(this).attr("data-all_parcours");
+            let parcours_id = $(this).attr("data-parcours_id");
+
+
+                $(":checkbox").each(function () {
+                    var ischecked = $(this).is(":checked");
+                    if (ischecked) {
+                        checkbox_value += $(this).val() + "-";
+                    }
+                });
+                
+
+                if (checkbox_value == "") { alert('Vous devez sélectionner au moins un parcours.') ;   return false ;} 
+
+                $.ajax(
+                    {
+                        type: "POST",
+                        dataType: "json",
+                        data: {
+                            'course_id': course_id,
+                            'parcours_id': parcours_id,
+                            csrfmiddlewaretoken: csrf_token,
+                            'checkbox_value' : checkbox_value,
+                            'all_parcours' : all_parcours,
+                        },
+                        url: "../parcours_clone_course",
+                        success: function (data) {
+
+                            $('#title_parcours'+parcours_id).html(data.success);
+                        }
+                    }
+                )
+         });
+
+
+
+
+
+
+
+
+
+
+
+
     });
 
 });
