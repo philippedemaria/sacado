dc1d5e5ae13093e429b7fedcd5235449e763ee33
Philippe Demaria <philippe.demaria83@gmail.com>
2022-01-19 18:22:55 +0100

    Update routing.py

--- sacado/routing.py
+++ sacado/routing.py
@@ -1,11 +1,11 @@
 from django.urls import path
 from qcm.consumers import TableauConsumer
-
+from tool.consumers import VisioConsumer
 
 ws_urlpatterns = [
 
-	path('qcm/tableau/', TableauConsumer.as_asgi())
-	
+	path('qcm/tableau/', TableauConsumer.as_asgi()),
+	path('tool/visiocast', VisioConsumer.as_asgi())
 ]
 
 
