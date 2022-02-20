eff724d19db660fe88fb8517da9ab06788670477
stephan ceroi <stephan.ceroi@gmail.com>
2022-02-20 21:56:37 +0100

    correction message profgeneral->eleve

--- sacado/asgi.py
+++ sacado/asgi.py
@@ -1,19 +1,23 @@
+"""
+ASGI entrypoint. Configures Django and then runs the application
+defined in the ASGI_APPLICATION setting.
+"""
+
 import os
-
-from django.core.asgi import get_asgi_application
-
-from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
-
-from .routing import ws_urlpatterns
+import django
+from channels.routing import get_default_application
+#from django.core.asgi import get_asgi_application
+#from channels.auth import AuthMiddlewareStack
+#from .routing import ws_urlpatterns
 
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sacado.settings")
 
-
-application = ProtocolTypeRouter({
-    # Django's ASGI application to handle traditional HTTP requests
-    "http": get_asgi_application() ,
-    # WebSocket handler
-    "websocket": AuthMiddlewareStack(  URLRouter(ws_urlpatterns)  ) 
-})
+django.setup()
+application= get_default_application()
+#application = ProtocolTypeRouter({
+# Django's ASGI application to handle traditional HTTP requests
+#"http": get_asgi_application() ,
+# WebSocket handler
+#    "websocket": AuthMiddlewareStack(  URLRouter(ws_urlpatterns)  )
+#})
