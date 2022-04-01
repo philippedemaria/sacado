93640e8e6ee84f19938cd51c61f82753d4cd7d5b
Philippe Demaria <philippe.demaria83@gmail.com>
2022-04-01 13:00:07 +0100

    maj

--- qcm/views.py
+++ qcm/views.py
@@ -69,10 +69,10 @@ def fill_the_skills(request):
 
     rs = Relationship.objects.filter(skills=None)
     nb = rs.count() 
-    relations = rs[:1000]
+    relations = rs[:2000]
     for r in relations:
         r.skills.set( r.exercise.supportfile.skills.all() )
-    context = { 'nb' : nb , }
+    context = { 'nb' : nb , 'relations' : relations  }
     return render(request, 'qcm/fill_the_skills_page.html', context )
 
 #################################################################
