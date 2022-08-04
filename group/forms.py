cc7a62c6624879428eceecb75adaf6ea97c8100f
Philippe Demaria <philippe.demaria83@gmail.com>
2022-08-04 19:22:04 +0100

    Update forms.py

--- group/forms.py
+++ group/forms.py
@@ -1,9 +1,17 @@
 from django import forms
 from .models import Group 
-
+from socle.models import Subject,  Level 
 
 
 class GroupForm(forms.ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        super(GroupForm, self).__init__(*args, **kwargs)
+        subjects = Subject.objects.filter(is_active=1)
+        self.fields['subjects']  = forms.ModelMultipleChoiceField(queryset=subjects)
+        levels = Level.objects.order_by("ranking")
+        self.fields['levels']  = forms.ModelMultipleChoiceField(queryset=levels)
+
     class Meta:
         model = Group 
         fields = ('name','color','level','assign','suiviparent','lock','subject','studentprofile' ) 
