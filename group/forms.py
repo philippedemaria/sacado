bd7748c3f3c9fea90278323796e617cb53b0b8bf
Philippe Demaria <philippe.demaria83@gmail.com>
2022-08-04 19:25:12 +0100

    Update forms.py

--- group/forms.py
+++ group/forms.py
@@ -8,9 +8,9 @@ class GroupForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super(GroupForm, self).__init__(*args, **kwargs)
         subjects = Subject.objects.filter(is_active=1)
-        self.fields['subjects']  = forms.ModelMultipleChoiceField(queryset=subjects)
+        self.fields['subject']  = forms.ModelMultipleChoiceField(queryset=subjects)
         levels = Level.objects.order_by("ranking")
-        self.fields['levels']  = forms.ModelMultipleChoiceField(queryset=levels)
+        self.fields['level']  = forms.ModelMultipleChoiceField(queryset=levels)
 
     class Meta:
         model = Group 
@@ -31,7 +31,7 @@ class GroupTeacherForm(forms.ModelForm):
 		if teacher:
 
 			subjects = teacher.subjects.all()
-			levels = teacher.levels.all()
+			levels = teacher.levels.order_by("ranking")
 			self.fields['subject']	 = forms.ModelChoiceField(queryset=subjects,  required=True)    
 			self.fields['level']	 = forms.ModelChoiceField(queryset=levels,  required=True)         
       
