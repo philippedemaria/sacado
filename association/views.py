bf92f988a7f925469b23443433af28c5b13708a5
Philippe Demaria <philippe.demaria83@gmail.com>
2022-08-05 02:35:21 +0100

    Update views.py

--- association/views.py
+++ association/views.py
@@ -434,7 +434,7 @@ def calcule_bank_bilan(request):
     accountings_sales_c = Accounting.objects.filter(date__gte = start_date  , date__lte = end_date,plan__code__gte=700, is_cpca = 0,is_credit=1).aggregate(Sum('amount'))
     accountings_sales_d = Accounting.objects.filter(date__gte = start_date  , date__lte = end_date,plan__code__gte=700, is_cpca = 0,is_credit=0).aggregate(Sum('amount'))
     try : 
-        accountings_sale  = accountings_sales_c["amount__sum"] - accountings_sales_d["amount__sum"]
+        accountings_sale  = accountings_sales_c["amount__sum"] + accountings_sales_d["amount__sum"]
     except :
         accountings_sale  = accountings_sales_c["amount__sum"]  
  
