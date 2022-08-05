25451a284ae83dd7bb9986397413b0eb0f52e7fa
Philippe Demaria <philippe.demaria83@gmail.com>
2022-08-05 03:36:42 +0100

    Update views.py

--- association/views.py
+++ association/views.py
@@ -496,20 +496,17 @@ def calcule_bank_bilan(request):
 
 
     accountings_paypal_c = Accounting.objects.filter(date__gte = start_date  , date__lte = end_date, is_paypal =1 ,  tp=0).exclude(date_payment=None).aggregate(Sum('amount'))
-    accountings_paypal_d = Accounting.objects.filter(date__gte = start_date  , date__lte = end_date,is_paypal =1 , tp=2).aggregate(Sum('amount'))
+    accountings_paypal_d = Accounting.objects.filter(date__gte = start_date  , date__lte = end_date, is_paypal = 1 , tp=2).aggregate(Sum('amount'))
     if accountings_paypal_c["amount__sum"] : accounting1 = accountings_paypal_c["amount__sum"] 
     else : accounting1 = 0 
     if accountings_paypal_d["amount__sum"] : accounting2 = accountings_paypal_d["amount__sum"] 
     else : accounting2 = 0 
 
-
     accountings_paypal  = accounting1 - accounting2
 
 
-    accountings_ca_d = Accounting.objects.filter(date__gte = start_date  , date__lte = end_date,is_paypal =0 , is_credit=0,tp=2).exclude(date_payment=None).aggregate(Sum('amount'))
-    accountings_ca_c = Accounting.objects.filter(date__gte = start_date  , date__lte = end_date, is_paypal =0 , is_credit=1, tp=2).exclude(date_payment=None).aggregate(Sum('amount'))
-    accountings_ca  = accountings_ca_c["amount__sum"]   - accountings_ca_d["amount__sum"]
-
+    accountings_ca_d = Accounting.objects.filter(date__gte = start_date  , date__lte = end_date, is_paypal = 0 , is_credit=0, tp=2).exclude(date_payment=None).aggregate(Sum('amount'))
+    accountings_ca_c = Accounting.objects.filter(date__gte = start_date  , date__lte = end_date, is_paypal = 0 , is_credit=1, tp=2).aggregate(Sum('amount'))
     if accountings_ca_c["amount__sum"] : accounting1 = accountings_ca_c["amount__sum"] 
     else : accounting1 = 0 
     if accountings_ca_d["amount__sum"] : accounting2 = accountings_ca_d["amount__sum"] 
