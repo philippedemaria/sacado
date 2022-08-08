806621b964ae5089f93b1a17b553b608eba8d321
Philippe Demaria <philippe.demaria83@gmail.com>
2022-08-08 08:59:28 +0100

    Update common_teacher_register.js

--- staticfiles/js/common_teacher_register.js
+++ staticfiles/js/common_teacher_register.js
@@ -6,6 +6,7 @@ requirejs.config({
         bootstrap: "lib/bootstrap.min",
         bootstrap_popover: ["https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.6/umd/popper.min", 'lib/popper.min'], 
         jquery: ['//ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min', 'lib/jquery-2.2.4.min'],
+        select2: "lib/select2.full.min",
         config_select2: "script/config-select2",
         multiselect: "lib/multiselect.min",
     },
