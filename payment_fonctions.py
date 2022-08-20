3a925c7784358eed89ff16dbbe69d46503bc7f81
Philippe Demaria <philippe.demaria83@gmail.com>
2022-08-20 17:39:14 +0100

    Update payment_fonctions.py

--- payment_fonctions.py
+++ payment_fonctions.py
@@ -11,7 +11,7 @@ def accounting_adhesion(school, today , date_payment, user, is_active , observat
     next_year = int(this_year) + 1
     objet     = "Adhésion "+ str(this_year) +" - " +str(next_year)
 
-    accounting, create = Accounting.objects.create( school = school   , is_active = 0 ,  date = today , amount= fee , objet = objet , beneficiaire =  school.name , chrono = create_chrono(Accounting,"FACTURE") ,
+    accounting = Accounting.objects.create( school = school   , is_active = 0 ,  date = today , amount= fee , objet = objet , beneficiaire =  school.name , chrono = create_chrono(Accounting,"FACTURE") ,
                                                                                                                     is_credit = 1 , address =  school.address , complement= school.complement , is_abonnement = True ,
                                                                                                                     town= school.town , country= school.country , contact= school.address , forme= "FACTURE", plan_id= 18 ,
                                                                                                                     observation= observation ,date_payment= date_payment , user= user )
