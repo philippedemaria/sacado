a8fad913e95dd049c7a424e0cdd76c82b5ca8244
Philippe Demaria <philippe.demaria83@gmail.com>
2022-08-30 22:41:54 +0100

    Update views.py

--- association/views.py
+++ association/views.py
@@ -636,7 +636,12 @@ def calcule_bank_bilan(request):
             pass
         plan_resultats.append( my_dico )
 
-    results = products - charges
+    try :
+        prev_year    = Activeyear.objects.get(is_active=1).year - 1  
+        resulat_prev = Activeyear.objects.get(year=prev_year).solde 
+        results      = products - charges + resulat_prev
+    except :
+        results = products - charges
     rs =  cs - ps
 
 
