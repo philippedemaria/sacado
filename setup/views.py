1dbdfcf1e086ab56466a550361c611d9fc6cafeb
Philippe Demaria <philippe.demaria83@gmail.com>
2022-09-02 16:29:02 +0100

    Update views.py

--- setup/views.py
+++ setup/views.py
@@ -360,7 +360,7 @@ def ressource_sacado(request): #Protection saml pour le GAR
                     elif code_level == 2212 : level_id = 11
                     else : level_id = 12
                     level = Level.objects.get(pk=level_id)
-                    Teacher.subjects.add(level)
+                    teacher.subjects.add(level)
 
 
                 for group in groups :
