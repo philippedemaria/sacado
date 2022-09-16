b11a2088adffebf5e99adc789d2875260bb22641
Philippe Demaria <philippe.demaria83@gmail.com>
2022-09-16 17:49:34 +0100

    Update views.py

--- school/views.py
+++ school/views.py
@@ -1148,16 +1148,16 @@ def csv_full_group(request):
         csv_file = request.FILES["csv_file"]
         if not csv_file.name.endswith('.csv'):
             messages.error(request, "Le fichier n'est pas format CSV")
-            return HttpResponseRedirect(reverse("csv_full_group"))
+            return redirect ("csv_full_group")
         # if file is too large, return
         if csv_file.multiple_chunks():
             messages.error(request, "Le fichier est trop lourd (%.2f MB)." % (csv_file.size / (1000 * 1000),))
-            return HttpResponseRedirect(reverse("csv_full_group"))
+            return redirect ("csv_full_group")
         try:
             file_data = csv_file.read().decode("utf-8")
         except UnicodeDecodeError:
             messages.error(request, 'Erreur..... Votre fichier contient des caractères spéciaux qui ne peuvent pas être décodés. Merci de vérifier que votre fichier .csv est bien encodé au format UTF-8.')
-            return HttpResponseRedirect(reverse("csv_full_group"))
+            return redirect ("csv_full_group")
 
         lines = file_data.split("\r\n")
         # loop over the lines and save them in db. If error , store as string and then display
