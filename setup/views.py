b4cc2a1a946964fade1af2d135dd3761663cbe31
Philippe Demaria <philippe.demaria83@gmail.com>
2022-09-21 16:23:55 +0100

    maj

--- setup/views.py
+++ setup/views.py
@@ -451,7 +451,7 @@ def ressource_sacado(request): #Protection saml pour le GAR
                                 pass
 
                         else :
-                            grp, creat = Group.objects.get_or_create(name = name ,  school = school , defaults = { 'subject_id' : 1 , 'teacher' : teacher ,   'level_id' : level_id , "lock" : 0  })
+                            grp, creat = Group.objects.get_or_create(name = name , teacher = teacher ,   school = school , defaults = { 'subject_id' : 1 ,   'level_id' : level_id , "lock" : 0  })
                             try :  # Profil élève
                                 if creat :
                                     username_student_profile  = username+"_e-test_"+str(uuid.uuid4())[:4]
