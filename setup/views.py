43be03c441304740925e2edfb9fea5d7ce2420ed
Philippe Demaria <philippe.demaria83@gmail.com>
2022-10-17 11:03:15 +0100

    Update views.py

--- setup/views.py
+++ setup/views.py
@@ -371,6 +371,7 @@ def ressource_sacado(request): #Protection saml pour le GAR
 
         if user_authenticated :
             login(request, user_authenticated,  backend='django.contrib.auth.backends.ModelBackend' )
+            user, created     = User.objects.get_or_create(username = username)
             request.session["user_id"] = user.id
             return redirect("index")
 
