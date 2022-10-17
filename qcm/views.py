f362ca65d8c68c0d1620ba9237971412f0c8c56f
Philippe Demaria <philippe.demaria83@gmail.com>
2022-10-17 16:00:17 +0100

    Update views.py

--- qcm/views.py
+++ qcm/views.py
@@ -8152,7 +8152,11 @@ def export_notes_after_evaluation(request):
         response['Content-Disposition'] = 'attachment; filename="etablissement.xls"'
 
         wb = xlwt.Workbook(encoding='utf-8')
-        ws = wb.add_sheet(parcours.title)
+
+        ptitle = parcours.title
+        if  len(parcours.title) > 15:
+            ptitle = parcours.title[:15]
+        ws = wb.add_sheet(ptitle)
 
         # Sheet header, first row
         row_num = 0
