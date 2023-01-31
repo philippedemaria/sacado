f2096215c747634306aacfacc5c5899ee765ca69
Philippe Demaria <philippe.demaria83@gmail.com>
2023-01-31 15:28:34 +0100

    maj

--- qcm/forms.py
+++ qcm/forms.py
@@ -135,8 +135,8 @@ class FolderForm(forms.ModelForm):
 		self.fields['coteachers'] = forms.ModelMultipleChoiceField(queryset=coteachers,  required=False)
 		
 
-		self.fields['level']    = forms.ModelMultipleChoiceField(queryset=teacher.levels.exclude(pk=13).order_by("ranking"),  required=False)
-		self.fields['subject']  = forms.ModelMultipleChoiceField(queryset=teacher.subjects.filter(is_active=1),  required=False)
+		self.fields['level']    = forms.ModelChoiceField(queryset=teacher.levels.exclude(pk=13).order_by("ranking"),  required=False)
+		self.fields['subject']  = forms.ModelChoiceField(queryset=teacher.subjects.filter(is_active=1),  required=False)
 		if subject and level :
 			parcours                = teacher.teacher_parcours.filter(subject=subject,level=level,is_trash=0,is_archive=0).order_by("title")
 			self.fields['parcours'] = forms.ModelMultipleChoiceField(queryset=parcours, widget=forms.CheckboxSelectMultiple, required=False)
