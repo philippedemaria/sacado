20d9a4c888d24262de9f035c3d6679cee6858da7
Philippe Demaria <philippe.demaria83@gmail.com>
2023-01-31 10:16:06 +0100

    maj

---
+++ staticfiles/js/script/ajax-exotex_create.js
@@ -0,0 +1,13 @@
+define(['jquery', 'bootstrap'], function ($) {
+    $(document).ready(function () {
+        console.log("chargement JS ajax-exercise.js OK");
+
+
+        $('[type=checkbox]').prop('checked', false);            
+        $('#id_is_share').prop('checked', true);  
+       
+
+});
+
+});
+
