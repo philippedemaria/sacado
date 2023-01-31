90c9a5195d48cbaa91b8d9d0a5fa687635d0903d
Philippe Demaria <philippe.demaria83@gmail.com>
2023-01-31 14:29:04 +0100

    Update ajax-exotex_create.js

--- staticfiles/js/script/ajax-exotex_create.js
+++ staticfiles/js/script/ajax-exotex_create.js
@@ -1,11 +1,11 @@
 define(['jquery', 'bootstrap'], function ($) {
     $(document).ready(function () {
-        console.log("chargement JS ajax-exercise.js OK");
+        console.log("chargement JS ajax-exotex_create.js OK");
 
 
         $('[type=checkbox]').prop('checked', false);            
         $('#id_is_share').prop('checked', true);  
-       
+        $('#id_calculator').prop('checked', true);
 
 });
 
