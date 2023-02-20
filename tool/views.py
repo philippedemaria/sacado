6ed42f92a7bbd4583a6c7e378869f7267213a865
Philippe Demaria <philippe.demaria83@gmail.com>
2023-02-21 00:02:41 +0100

    Update views.py

--- tool/views.py
+++ tool/views.py
@@ -2191,7 +2191,7 @@ def print_qf_to_pdf(request):
 
                 if question.imagefile :
                     elements += r" \includegraphics[scale=0.5]{"+question.imagefile.url+r"}"
-                if 'Complète' in question.title or 'complète' in question.title : elements += r"\\ \hline"
+                if 'Complète' in question.filltheblanks or 'complète' in question.filltheblanks : elements += r"\\ \hline"
                 else : elements += r"\\ & {\scriptsize Écris ta réponse :} \vspace{1.2cm} \\ \hline"
             elements += r"\end{tabular}\end{minipage}"
 
