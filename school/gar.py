95dde9a7b1ac6fec175d0f5bd183eec41e1f94c6
Philippe Demaria <philippe.demaria83@gmail.com>
2023-06-03 19:59:26 +0100

    Update gar.py

--- school/gar.py
+++ school/gar.py
@@ -48,7 +48,7 @@ def web_abonnement_xml(accounting,id_abonnement , today):
 
 def web_update_abonnement_xml(customer,id_abonnement):
     #Webservice du GAR
-    date_start, date_stop = customer.date_start_gar.isoformat().split("T"), date_stop.isoformat().split("T") 
+    date_start, date_stop = customer.date_start_gar.isoformat().split("T"), customer.date_stop.isoformat().split("T") 
     body = "<?xml version='1.0' encoding='UTF-8'?>"
     body += "<abonnement xmlns='http://www.atosworldline.com/wsabonnement/v1.0/'>"
     body += "<idAbonnement>" + id_abonnement +"</idAbonnement>"
