ac7b1f6dc6338bf913d9e540db13b4ab2f0c08c2
Philippe Demaria <philippe.demaria83@gmail.com>
2023-09-11 23:08:46 +0100

    Update views.py

--- bibliotex/views.py
+++ bibliotex/views.py
@@ -721,7 +721,7 @@ def div_to_display_latex(request):
     # pour windows
     # file_path = settings.DIR_TMP_TEX+r"\\doc" 
     # pour le serveur Linux
-    file_path = settings.DIR_TMP_TEX + str(request.user.id)+"_bliotex_display"
+    file_path = settings.DIR_TMP_TEX + str(request.user.id)+"_bibliotex_display"
     ################################################################# 
     ################################################################# 
     with open(file_path+".tex", 'w') as file:
@@ -811,7 +811,7 @@ def exotex_display_pdf(request,ide):
     # pour windows
     # file_path = settings.DIR_TMP_TEX+r"\\doc" 
     # pour le serveur Linux
-    file_path = settings.DIR_TMP_TEX + str(request.user.id)+"_bliotex_display"
+    file_path = settings.DIR_TMP_TEX + str(request.user.id)+"_bibliotex_display"
     ################################################################# 
     ################################################################# 
     with open(file_path+".tex", 'w') as file:
