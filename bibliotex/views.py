a103efcda995414e78eb1e7b22aee799b7c4b1b2
Philippe Demaria <philippe.demaria83@gmail.com>
2023-09-12 08:49:33 +0100

    Update views.py

--- bibliotex/views.py
+++ bibliotex/views.py
@@ -721,7 +721,7 @@ def div_to_display_latex(request):
     # pour windows
     # file_path = settings.DIR_TMP_TEX+r"\\doc" 
     # pour le serveur Linux
-    file_path = settings.DIR_TMP_TEX + str(request.user.id)+"_bibliotex_display"
+    file_path = settings.DIR_TMP_TEX + str(request.user.id)+"_exotex_display"
     ################################################################# 
     ################################################################# 
     with open(file_path+".tex", 'w') as file:
@@ -738,7 +738,7 @@ def div_to_display_latex(request):
     else : 
         if os.path.isfile(file_path+".out"):os.remove(file_path+".out")
         if os.path.isfile(file_path+".aux"):os.remove(file_path+".aux")     
-        return FileResponse(open("/var/www/sacado/ressources/tex/tmp_tex/"+ str(request.user.id)+"_bibliotex_display.pdf", 'rb'))
+        return FileResponse(open("/var/www/sacado/ressources/tex/tmp_tex/"+ str(request.user.id)+"_exotex_display.pdf", 'rb'))
 
  
  
