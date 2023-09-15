804f1b9e0b99892b2587231c19245ed414edc7ee
Philippe Demaria <philippe.demaria83@gmail.com>
2023-09-15 21:00:11 +0100

    Update ajax-bibliotex.js

--- staticfiles/js/script/ajax-bibliotex.js
+++ staticfiles/js/script/ajax-bibliotex.js
@@ -288,6 +288,7 @@ define(['jquery', 'bootstrap', 'ui', 'ui_sortable'], function ($) {
     $('body').on('change', "#customRange", function (e) {
             size  = $("#customRange").val() ; 
             $("#projection_div").attr("style","font-size:"+size+"rem");
+            $("#projection_div").find("td").attr("style","font-size:"+size+"rem");
         });
 
 
