d186d50debbcfab7d5a4e7d473c323c875cee52f
Philippe Demaria <philippe.demaria83@gmail.com>
2023-10-05 16:11:36 +0100

    maj

--- association/urls.py
+++ association/urls.py
@@ -120,11 +120,11 @@ urlpatterns = [
     path('to_clean_database/<int:idl>/<int:start>', to_clean_database, name='to_clean_database'),
 
 
-
-
-
     path('create_bibliotex_from_tex', create_bibliotex_from_tex, name='create_bibliotex_from_tex'),
     path('create_exotex_from_tex', create_exotex_from_tex, name='create_exotex_from_tex'),
+
+
+    path('toplog', toplog, name='toplog'),
 
 ]
  
