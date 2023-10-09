39142bff27a9eb2c020e0ed51a6ab563a0674978
Philippe Demaria <philippe.demaria83@gmail.com>
2023-10-09 23:26:40 +0100

    maj

--- qcm/views.py
+++ qcm/views.py
@@ -9011,10 +9011,10 @@ def store_the_score_relation_ajax(request):
                 pass
             
             timer_mathis = time.time() - mathis_time 
-            if timer_mathis > 1 :
-                f=open("/var/www/sacado/logs/mysql.log",'a')
-                print(""+str( datetime.now() )+" -  time to save an exercice " +student.user.last_name+" - " +student.user.first_name+" - " +student.user.school.country.name+"  : "+str(timer_mathis) , file=f)
-                f.close()
+
+            f=open("/var/www/sacado/logs/mysql.log",'a')
+            print(""+str( datetime.now() )+" -  time to save an exercice " +student.user.last_name+" - " +student.user.first_name+" - " +student.user.school.country.name+"  : "+str(timer_mathis) , file=f)
+            f.close()
 
             try :
                 nb_done = 0
