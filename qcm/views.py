1af18a31ae58ba022c63f8ee72b5bf1e58050682
Philippe Demaria <philippe.demaria83@gmail.com>
2023-10-09 23:29:39 +0100

    Update views.py

--- qcm/views.py
+++ qcm/views.py
@@ -9013,7 +9013,7 @@ def store_the_score_relation_ajax(request):
             timer_mathis = time.time() - mathis_time 
 
             f=open("/var/www/sacado/logs/mysql.log",'a')
-            print(""+str( datetime.now() )+" -  time to save an exercice " +student.user.last_name+" - " +student.user.first_name+" - " +student.user.school.country.name+"  : "+str(timer_mathis) , file=f)
+            print(""+str( datetime.now() )+" -  time to save an exercice,  student_id : " +str(student.user.id)+" - " +student.user.last_name+" - " +student.user.first_name+" - " +student.user.school.country.name+"  : "+str(timer_mathis) , file=f)
             f.close()
 
             try :
