89274b1554b478228d790e02ee9dadece2d5408e
Philippe Demaria <philippe.demaria83@gmail.com>
2023-10-25 01:04:40 +0100

    Update views.py

--- book/views.py
+++ book/views.py
@@ -1040,7 +1040,7 @@ def print_latex_to_pdf(request,idch,idp):
         for page in chapter.pages.filter(is_publish=1).order_by("number"):
             if page.paragraphs.count()>0 :
                 elements +=  r'{\huge '+ page.title+r'} \hfill '+str(chapter.book.level.shortname)+". "+  chapter.title
-                elements +=  r" \hrule \vspace {0.1cm}"
+                elements +=  r" \hrule"
             for paragraph in page.paragraphs.order_by("ranking"):
                 if 'Cours' in page.title : elements += r'\section{'+paragraph.title+r'}'
                 elif paragraph.number > 0 : elements += r'\section*{'+paragraph.title+r'}' 
@@ -1058,7 +1058,7 @@ def print_latex_to_pdf(request,idch,idp):
     elif idp :
         page = Page.objects.get(pk=idp)
         elements +=  r'{\huge '+ page.title+r'}'
-        elements +=  r" \hrule \vspace {0.1cm}"
+        elements +=  r" \hrule"
         for paragraph in page.paragraphs.order_by("ranking"):
             if 'Cours' in page.title : elements += r'\section{'+paragraph.title+r'}'
             elif paragraph.number > 0 : elements += r'\section*{'+paragraph.title+r'}' 
@@ -1109,16 +1109,12 @@ def print_latex_to_tex(request,idch,idp):
         for page in chapter.pages.filter(is_publish=1).order_by("number"):
             if page.paragraphs.count()>0 :
                 elements +=  r'{\huge '+ page.title+r'} \hfill '+str(chapter.book.level.shortname)+". "+  chapter.title
-                elements +=  r" \hrule \vspace {0.1cm}"
+                elements +=  r" \hrule"
             for paragraph in page.paragraphs.order_by("ranking"):
                 if 'Cours' in page.title : elements += r'\section{'+paragraph.title+r'}'
                 elif paragraph.number > 0 : elements += r'\section*{'+paragraph.title+r'}' 
  
                 for bloc in paragraph.blocs.order_by("ranking"):
-
-                    exercises = bloc.exercises.all() 
-                    exotexs = bloc.exotexs.all()
-                    appliquettes = bloc.appliquettes.all()
 
                     if bloc.size != 12 :
                         elements += r"\begin{minipage}{"+str(round(bloc.size/12 - 0.02,1)).replace(",",".") +r"\linewidth}"
@@ -1135,22 +1131,16 @@ def print_latex_to_tex(request,idch,idp):
     elif idp :
         page = Page.objects.get(pk=idp)
         elements +=  r'{\huge '+ page.title+r'}'
-        elements +=  r" \hrule \vspace {0.1cm}"
+        elements +=  r" \hrule"
         for paragraph in page.paragraphs.order_by("ranking"):
             if 'Cours' in page.title : elements += r'\section{'+paragraph.title+r'}'
             elif paragraph.number > 0 : elements += r'\section*{'+paragraph.title+r'}' 
 
             for bloc in paragraph.blocs.order_by("ranking"):
-
-                exercises = bloc.exercises.all() 
-                exotexs = bloc.exotexs.all()
-                appliquettes = bloc.appliquettes.all()
-
                 if bloc.size != 12 :
                     elements += r"\begin{minipage}{"+str(round(bloc.size/12 - 0.02,1)).replace(",",".") +r"\linewidth}"
                     elements +=  bloc.typebloc_latex()
                     elements += r"\end{minipage}\hfill"
-
 
                 else : 
                     elements +=  bloc.typebloc_latex()
