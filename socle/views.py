2c45057f87d06bef618824d7a39b86d764be263b
Philippe Demaria <philippe.demaria83@gmail.com>
2023-12-20 13:57:28 +0100

    Update views.py

--- socle/views.py
+++ socle/views.py
@@ -33,14 +33,14 @@ def export_to(request,modelchoice):
                     else : image = str(theme.image)
                 except :
                     image = ""
-                themetab.append({'title':theme.name ,'image': image , 'subjectId':theme.subject.id , 'levelId':dataLevel[w.level.id] })
+                themetab.append({ id:theme.id, title :theme.name , image : image ,  subjectId :theme.subject.id ,  levelId :dataLevel[w.level.id] })
 
     elif modelchoice == 2 : 
 
         themes = Waiting.objects.order_by('id')
         themetab = []
         for theme in themes :
-            themetab.append({'title':theme.name , 'themeId':theme.id  })
+            themetab.append({ id:theme.id, title :theme.name ,  themeId :theme.id  })
      
 
     elif modelchoice == 3 :
@@ -49,14 +49,14 @@ def export_to(request,modelchoice):
         themetab = []
         for theme in themes :
             if theme.waiting :
-                themetab.append({'title':theme.name ,'waitingId':theme.waiting.id  })
+                themetab.append( { id:theme.id, title:theme.name , waitingId :theme.waiting.id  })
 
     elif modelchoice == 4 : 
 
         themes = Skill.objects.order_by('subject')
         themetab = []
         for theme in themes :
-            themetab.append({'title':theme.name , 'subjectId':dataLevel[w.level.id]})
+            themetab.append({ id:theme.id, title : theme.name ,  subjectId :dataLevel[w.level.id]})
 
     return render(request, 'socle/export_to.html', {'themetab': themetab,  })
  
